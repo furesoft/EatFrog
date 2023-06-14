5c4dd8682a0a4ceb2cda799c73e318ebda555b93
Chris <filmee24@gmail.com>
2023-06-14 19:59:23 +0200

    feat: Add Core For Assembler Language

---
+++ Src/EatFrog/EatFrog.Assembler.Core/Class1.cs
@@ -0,0 +1,12 @@
+﻿using Furesoft.PrattParser;
+using Furesoft.PrattParser.Nodes;
+
+namespace EatFrog.Assembler.Core;
+
+public class AssemblyParser : Parser<AstNode>
+{
+    protected override void InitLexer(Lexer lexer)
+    {
+        
+    }
+}
