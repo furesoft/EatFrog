7a55005765e3b605b5a42395b4cd6fe6a4cd9719
Chris <filmee24@gmail.com>
2023-06-15 19:17:16 +0200

    feat: Add BytecodeEmitter

---
+++ Src/EatFrog/EatFrog/BytecodeEmitter.cs
@@ -0,0 +1,24 @@
+﻿namespace EatFrog;
+
+public abstract class BytecodeEmitter<TEncoder, TOpcode>
+    where TEncoder : InstructionEncoder<TOpcode>, new()
+    where TOpcode : struct
+{
+    public abstract ulong StackStartAddress { get; }
+
+    public bool Emit(Stream target, IEnumerable<Instruction<TOpcode>> instructions)
+    {
+        var encoder = new TEncoder();
+        using var binaryWriter = new BinaryWriter(target);
+
+        foreach (var instruction in instructions)
+        {
+            if (!encoder.Encode(instruction, binaryWriter))
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
