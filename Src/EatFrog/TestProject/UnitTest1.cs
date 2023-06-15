e4e056bf20233778a460174ca528ad1d865f2412
Chris <filmee24@gmail.com>
2023-06-15 07:41:31 +0200

    chore(deps): Update Packages

--- Src/EatFrog/TestProject/UnitTest1.cs
+++ Src/EatFrog/TestProject/UnitTest1.cs
@@ -1,6 +1,4 @@
-using EatFrog.Assembler.Core;
 using Furesoft.PrattParser;
-using Furesoft.PrattParser.Nodes;
 using AParser = EatFrog.Assembler.Core.AssemblyParser<TestProject.TestOpcodes>;
 
 namespace TestProject;
@@ -16,7 +14,7 @@ public class ParsingTests
     [Test]
     public void Opcode_Should_Pass()
     {
-        var tree = Parser.Parse<AstNode, AParser>("call 42,5", "test.dsl");
+        var tree = Parser.Parse<AParser>("call 42,5", "test.dsl");
         
         Assert.IsFalse(tree.Document.Messages.Any());
     }
