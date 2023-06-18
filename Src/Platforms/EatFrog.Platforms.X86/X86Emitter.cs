34220290a2cfca28aa9110d1ea585f22b83af54d
Chris <filmee24@gmail.com>
2023-06-18 19:15:51 +0200

    feat: Add x86 Endianess

--- Src/Platforms/EatFrog.Platforms.X86/X86Emitter.cs
+++ Src/Platforms/EatFrog.Platforms.X86/X86Emitter.cs
@@ -5,7 +5,7 @@ namespace EatFrog.Platforms.X86;
 public class X86Emitter : BytecodeEmitter<X86InstructionEncoder, X86InstructionValidator, 
     X86Opcode, X86Register, X86AddressEncoder>
 {
-    public X86Emitter(Stream target, Endian endian) : base(target, endian)
+    public X86Emitter(Stream target) : base(target, Endian.Little)
     {
     }
 
