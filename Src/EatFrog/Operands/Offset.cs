e1c1bec24f5eba6446381f42b5eb645573ba9eae
Chris <filmee24@gmail.com>
2023-06-25 06:57:48 +0200

    feat: Add Offset Operand

---
+++ Src/EatFrog/Operands/Offset.cs
@@ -0,0 +1,16 @@
+﻿namespace EatFrog.Operands;
+
+public class Offset : Operand
+{
+    public long Value { get; }
+
+    public Offset(long value)
+    {
+        Value = value;
+    }
+
+    public override string ToString()
+    {
+        return $"{Value}";
+    }
+}
