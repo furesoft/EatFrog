2402be5cb0a27b94a03144bb4e533888bb979bf0
Chris <filmee24@gmail.com>
2024-05-26 13:52:19 +0000

    Implemented X86 Address Encoding

--- Src/Platforms/EatFrog.Platforms.X86/X86AddressEncoder.cs
+++ Src/Platforms/EatFrog.Platforms.X86/X86AddressEncoder.cs
@@ -1,9 +1,20 @@
 ﻿namespace EatFrog.Platforms.X86;
 
+using System;
+
 public class X86AddressEncoder : IAddressEncoder
 {
     public byte[] Encode(ulong address)
     {
-        throw new NotImplementedException();
+        byte[] encodedAddress = new byte[8]; // Assuming x86 addresses are 8 bytes
+
+        // Convert the ulong address into a byte array
+        for (int i = 0; i < 8; i++)
+        {
+            encodedAddress[i] = (byte)(address & 0xFF); // Extract the least significant byte
+            address >>= 8; // Shift right to get the next byte
+        }
+
+        return encodedAddress;
     }
 }
